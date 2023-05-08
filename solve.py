7e445c02a90f61b9636593bae825e144ae4b26c2
Damzgod <130247514+Damzgod@users.noreply.github.com>
2023-05-08 23:59:47 +0430

    Update solve.py

--- solve.py
+++ solve.py
@@ -24,6 +24,35 @@ if d.upper() =="M":
         x = int(input("length: "))
         y = int(input("breadth: "))
         print(solve.exp(x, y))
+       
+       
+       from phy import Physics
+solve = Physics()
+d = input("Will you like to perform (M)aths or (P)hysics?: ")
+if d.upper() =="P":
+
+    g = input("Do you want to calculate (m)ass, (v)elocity, (d)istance, (s)peed, (f)orce:  ")
+    if g.upper() == "M":
+        x = int(input("type Force: "))
+        y = int(input("Acceleration: "))
+        print(solve.mass(x, y))
+    elif g.upper() == "V":
+        x = int(input("type displacement: "))
+        y = int(input("time: "))
+        print(solve.velocity(x, y))
+    elif g.upper() == "D":
+        x = int(input("type Speed: "))
+        y = int(input("Time: "))
+        print(solve.distance(x, y))
+    elif g.upper() == "S":
+        x = int(input("type distance: "))
+        y = int(input("type time: "))
+        print(solve.speed(x, y))
+    elif g.upper() == "F":
+        x = int(input("mass: "))
+        y = int(input("acceleration: "))
+        print(solve.Force(x, y))
+
 
 
 
